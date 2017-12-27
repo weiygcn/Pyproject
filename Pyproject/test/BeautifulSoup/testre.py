a9af580ade6943aab9d923a02789ecf9b6785ef9
weiygcn <weiygcn@163.com>
2017-12-27 16:49:31 +0800

    将公司与家里的学习代码合并入一个项目

--- Pyproject/test/BeautifulSoup/testre.py
+++ Pyproject/test/BeautifulSoup/testre.py
@@ -1,18 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
-class test:
+def test():
     url = 'http://www.cnblogs.com/yoyoketang/mvc/blog/sidecolumn.aspx?blogApp=yoyoketang'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'}
     session = requests.session()
-    r = session.get(url, headers=headers, verify=True)
+    r = session.get(url, headers=headers, verify=False)
     soupObj = BeautifulSoup(r.content, 'html.parser')
-    content = soupObj.find_all(class_='catListTag')
-    for i in content:
-        print(i.text,end='\t\n')
-        print(dir(i))
+    c = soupObj.find_all(class_='catListTitle')
+    print(c)
+    patten = re.compile('<li><a href="(.*?)">(.*?)</a></li>', re.S)
+    tags = re.findall(patten, str(c))
+    print(tags)
+    for a in tags:
+        print(a)
+        print(a[1])
 
 
 if __name__ == '__main__':
